f9fe4e2b64bf446f455e573a2ff527e248d8a5a0
pegCalde <50577515+pegCalde@users.noreply.github.com>
2021-04-06 11:25:02 +0200

    Update TestDropdownViewModel.cs

--- ViewModel/TestDropdownViewModel.cs
+++ ViewModel/TestDropdownViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 
-namespace EnvWinProjectQuizz.Model
+namespace PNRG-AppVacances.Model
 {
     class TestDropdownViewModel
     {
