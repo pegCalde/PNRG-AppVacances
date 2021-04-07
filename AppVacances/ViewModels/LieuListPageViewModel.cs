d6dc32a8a03117ae5f79484ce2c02bec35248b3d
GuillaumeSavin <guisav1996@hotmail.fr>
2021-04-07 20:28:11 +0200

    [structure] Create a sane base project

---
+++ AppVacances/ViewModels/LieuListPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppVacances
+{
+    public class LieuListPageViewModel : BaseViewModel
+    {
+        public LieuListPageViewModel()
+        {
+
+        }
+
+    }
+}
---
+++ AppVacances/Views/LieuDetailsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AppVacances
+{
+    public partial class LieuDetailsPage : ContentPage
+    {
+        public LieuDetailsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
