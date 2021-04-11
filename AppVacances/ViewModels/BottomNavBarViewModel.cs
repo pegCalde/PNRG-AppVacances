453eb28a29ddb32c4dc0d14a4c817fbcc061d973
pegCalde <calderonpeg@gmail.com>
2021-04-11 15:08:30 +0200

    base navBar. Besoin des pages finies pr vérifier la nav

---
+++ AppVacances/ViewModels/BottomNavBarViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppVacances.ViewModels
+{
+    public class BottomNavBarViewModel : TabbedPage
+    {
+		public BottomNavBarViewModel()
+		{
+			/*var navigationPage = new NavigationPage(new PageDetails());
+			navigationPage.Title = "Details";
+
+			Children.Add(new PageAccueil());
+			Children.Add(navigationPage);
+			Children.Add(new PageFavoris());*/
+		}
+	}
+}
---
+++ AppVacances/Views/BottomNavBar.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppVacances
+{
+    public partial class BottomNavBar : TabbedPage
+    {
+
+        public BottomNavBar()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
