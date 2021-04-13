90aa4384ec0da03c812f433f1ff8efbdb1f56ba0
Raoua Messai <raoua.messai@busit.com>
2021-04-13 09:49:38 +0200

    add meteo API

--- AppVacances/ViewModels/LieuDetailsPageViewModel.cs
+++ AppVacances/ViewModels/LieuDetailsPageViewModel.cs
@@ -1,6 +1,9 @@
-﻿using AppVacances.Service;
+﻿using AppVacances.Models;
+using AppVacances.Service;
+using Newtonsoft.Json;
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
